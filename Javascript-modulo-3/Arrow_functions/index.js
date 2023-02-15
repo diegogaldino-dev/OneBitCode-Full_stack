303256ebbf88de5b8a8751b601660238713a57c3
Diego Galdino <diegogaldino-dev@gmail.com>
2023-02-15 19:20:18 -0300

    reinforcing knowledge in arrow function

---
+++ Javascript-modulo-3/Arrow_functions/index.js
@@ -0,0 +1,35 @@
+function normalSum(a,b){
+  return a + b
+}
+
+console.log(`Soma normal: ${normalSum(2,2)}`)
+
+
+const anonymousSum = function(a, b){
+  return a + b
+}
+
+console.log(`Soma anonima: ${anonymousSum(2,2)}`)
+
+
+const arrowSum = (a,b) =>{
+  return a + b
+
+}
+
+console.log(`Soma arrow function: ${arrowSum(2,2)}`)
+
+const subtrac = (a,b) => a - b
+
+console.log(`Subtração: ${subtrac(5,2)}`)
+
+const double = n => `O dobro de ${n} é ${n * 2} `
+
+const number = 21
+console.log(double(number))
+
+const towns = ['Prontera', 'Izlude', 'Payon', 'Alberta', 'Geffen', 'Morroc']
+
+const startingWithP = towns.filter(town => town[0] === 'P')
+
+console.log(startingWithP)
