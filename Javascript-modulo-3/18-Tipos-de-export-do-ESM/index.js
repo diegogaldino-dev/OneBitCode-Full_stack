ebefb32606efe33fee5485e3498a36dee351ee6b
Diego Galdino <diegogaldino-dev@gmail.com>
2023-02-23 17:43:26 -0300

    tipos de exports

---
+++ Javascript-modulo-3/18-Tipos-de-export-do-ESM/index.js
@@ -0,0 +1,10 @@
+import {inline} from './inline.js'
+import defaultInline from './inline.js' 
+import {group} from './non-inline.js'
+import exportDefault from './non-inline.js'
+
+inline()
+defaultInline()
+
+group()
+exportDefault()
