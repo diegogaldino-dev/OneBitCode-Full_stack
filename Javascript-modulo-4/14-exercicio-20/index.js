a40da31fd77a60835b4abd927e94d1de6a49403a
Diego Galdino <diegogaldino-dev@gmail.com>
2023-03-08 19:33:13 -0300

    Exercício 20

---
+++ Javascript-modulo-4/14-exercicio-20/index.js
@@ -0,0 +1,30 @@
+import {Component} from './Component.js'
+import { Form } from './Form.js'
+import { Input } from './Input.js'
+import { Label } from './Label.js'
+
+const title = new Component('h1', 'body', {textContent: 'Ola mundo!'})
+console.log(title)
+title.render()
+
+title.tag = 'h3'
+
+title.build().render()
+
+const form = new Form('body')
+
+const label = new Label('Nome:', form, {htmlFor: 'nameInput'})
+const input = new Input(form, {id: 'nameInput', name: 'name'})
+
+
+form.render()
+label.render()
+
+form.addChildren(input)
+form.addChildren(
+  new Component('br'),
+  new Component('br'),
+  new Label('Data de nascimento:', {htmlFor: 'birthdayInput'}),
+  new Input (form, {id: 'birthdayInput', name: 'birthday', type: 'date'})
+
+)
