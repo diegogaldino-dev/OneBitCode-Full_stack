c35c4ff0e35e199a1573f23678613bf4ee7df976
Diego Galdino <diegogaldino-dev@gmail.com>
2023-03-08 16:08:54 -0300

    Encapsulamento

---
+++ Javascript-modulo-4/11-encapsulamento/Account.js
@@ -0,0 +1,32 @@
+class Account {
+  #password
+  #balance = 0
+
+  constructor(user){
+    this.email = user.email
+    this.#password = user.password
+  }
+
+  getBalance(email, password){
+    if(this.#authenticate(email, password)) {
+      return this.#balance
+    }else{
+      return null
+    }   
+  }
+
+  #authenticate(email, password){
+    return this.email === email && this.#password === password
+  }
+}
+
+const user = {
+  email: "diego@gmail.com",
+  password: "1234"
+}
+
+const myAccount = new Account(user)
+
+console.log(myAccount)
+
+console.log(myAccount.getBalance("diego@gmail.com", "1234"))
