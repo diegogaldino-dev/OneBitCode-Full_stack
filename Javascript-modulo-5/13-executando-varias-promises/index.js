397d4076da53420797128268d8675aac2a500d2b
Diego Galdino <diegogaldino-dev@gmail.com>
2023-03-14 13:21:56 -0300

    Funções async

--- Javascript-modulo-5/13-executando-varias-promises/index.js
+++ Javascript-modulo-5/13-executando-varias-promises/index.js
@@ -29,12 +29,12 @@
 
 const numbers = [4, 9, 5, 13, 77]
 
-function asyncSquare(x){
-  return new Promise((resolve, reject) => {
-    resolve(x * x)
-  })
+function asyncSquare(x) {
+    return new Promise((resolve, reject) => {
+        resolve(x * x)
+    })
 }
 
 Promise.all(numbers.map(number => asyncSquare(number))).then(squares => {
-  console.log(squares)
+    console.log(squares)
 })
