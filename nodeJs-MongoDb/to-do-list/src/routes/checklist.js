0e432897f9120b411cdc86050a1e099a1b542608
Diego Galdino <diegogaldino-dev@gmail.com>
2023-03-17 16:55:41 -0300

    Rota POST

--- nodeJs-MongoDb/to-do-list/src/routes/checklist.js
+++ nodeJs-MongoDb/to-do-list/src/routes/checklist.js
@@ -6,6 +6,10 @@ router.get('/', (req, res) => {
   console.log('Olá')
   res.send()
 })
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.status(200).json(req.body)
+})
 
 
 module.exports = router
