85d2c106237f176c1a0df4d08b8add848a7c74b2
Diego Galdino <diegogaldino-dev@gmail.com>
2023-03-17 13:54:36 -0300

    Middlewares

--- nodeJs-MongoDb/to-do-list/app.js
+++ nodeJs-MongoDb/to-do-list/app.js
@@ -2,11 +2,22 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
+const log = (req, res, next) => {
+  console.log(req.body)
+  console.log(`Data ${Date.now()}`)
+  next()
+}
+
+app.use(log)
+
 app.get('/', (req, res) =>{
   res.send('<h1>Minha lista de tarefas</h1>')
 })
 
 app.get('/json', (req, res) =>{
+  console.log(req.body)
   res.json({title: 'Tarefa X', done: true})
 })
 
