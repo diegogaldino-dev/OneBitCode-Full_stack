ff1af5eaa7e373115b8a0ee650157282517aa351
Diego Galdino <diegogaldino-dev@gmail.com>
2023-03-21 14:50:50 -0300

    Mapemento Objeto-Documento

--- nodeJs-MongoDb/to-do-list/config/database.js
+++ nodeJs-MongoDb/to-do-list/config/database.js
@@ -1,6 +1,10 @@
-const mongoose = require('mongoose')
-mongoose.Promise = global.Promise
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/todo-list', {useNewUrlParser: true, useUnifiedTopology: true})
-  .then(() => console.log('Conectando ao MongoDB'))
-  .catch((err) => console.log(err))
+mongoose
+  .connect("mongodb://localhost/todo-list", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Conectando ao MongoDB"))
+  .catch((err) => console.log(err));
