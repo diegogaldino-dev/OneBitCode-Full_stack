681721a32964fa9f929a1b1b7617137eebafb11d
Diego Galdino <diegogaldino-dev@gmail.com>
2023-03-21 17:02:02 -0300

    EJS e configuração com o Express

--- nodeJs-MongoDb/to-do-list/app.js
+++ nodeJs-MongoDb/to-do-list/app.js
@@ -1,12 +1,23 @@
 const express = require('express')
+const path = require('path')
+
 const checkListRouter = require('./src/routes/checklist')
+const rootRouter = require('./src/routes/index')
+
+
+
 require('./config/database')
 
 const app = express()
 
 app.use(express.json())
 
+app.set('views', path.join(__dirname, 'src/views'))
+app.set('view engine', 'ejs')
+
+app.use('/', rootRouter)
 app.use('/checklist', checkListRouter)
+
 
 app.listen(3000,() =>{
   console.log('Servidor foi iniciado')
