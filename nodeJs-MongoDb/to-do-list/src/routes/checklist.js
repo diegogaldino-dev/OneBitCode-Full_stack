309ac4650abed67889a7c6b18cb2522da66c557c
Diego Galdino <diegogaldino-dev@gmail.com>
2023-03-21 16:28:54 -0300

    Atualização e Remoção de Dados

--- nodeJs-MongoDb/to-do-list/src/routes/checklist.js
+++ nodeJs-MongoDb/to-do-list/src/routes/checklist.js
@@ -34,14 +34,25 @@ router.get('/:id', async (req, res) => {
   }
 })
 
-router.put('/:id', (req, res) => {
-  console.log(req.params.id)
-  res.send(`PUT ID: ${req.params.id}`)
+router.put('/:id', async(req, res) => {
+  let {name} = req.body
+  try {
+    let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new: true})    
+    res.status(200).json(checklist)    
+  } catch (error) {
+    res.status(422).json(error)   
+  }
+  
 })
 
-router.delete('/:id', (req, res) => {
-  console.log(req.params.id)
-  res.send(`DELETE: ${req.params.id}`)
+router.delete('/:id', async (req, res) => {
+  try {
+    let checklist = await Checklist.findByIdAndRemove(req.params.id)     
+    res.status(200).json(checklist)   
+
+  } catch (error) {
+    res.status(422).json(error)       
+  }  
 })
 
 
