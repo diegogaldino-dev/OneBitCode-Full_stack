94c0fb235f40f79d84553f296f9d1f3eddad6a52
Diego Galdino <diegogaldino-dev@gmail.com>
2023-03-21 15:54:04 -0300

    Listando dados do Model

--- nodeJs-MongoDb/to-do-list/src/routes/checklist.js
+++ nodeJs-MongoDb/to-do-list/src/routes/checklist.js
@@ -4,9 +4,13 @@ const router = express.Router()
 
 const Checklist = require('../models/checklist')
 
-router.get('/', (req, res) => {
-  console.log('Olá')
-  res.send()
+router.get('/', async (req, res) => {
+  try {
+    let checklist = await Checklist.find({})  
+    res.status(200).json(checklist)    
+  } catch (error) {
+    res.status(500).json(error)      
+  }
 })
 
 router.post('/', async (req, res) => {
@@ -16,15 +20,18 @@ router.post('/', async (req, res) => {
     let checklist = await Checklist.create({ name })
     res.status(200).json(checklist)    
   } catch (error) {
-    res.status(422).json(error)
-    
+    res.status(422).json(error)    
   }
   
 })
 
-router.get('/:id', (req, res) => {
-  console.log(req.params.id)
-  res.send(`ID: ${req.params.id}`)
+router.get('/:id', async (req, res) => {
+  try {
+    let checklist = await Checklist.findById(req.params.id) 
+    res.status(200).json(checklist)    
+  } catch (error) {
+    res.status(422).json(error)      
+  }
 })
 
 router.put('/:id', (req, res) => {
