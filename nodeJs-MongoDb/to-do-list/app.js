713e96cfb08eb19188f8f04e66e7bdb72c0851ac
Diego Galdino <diegogaldino-dev@gmail.com>
2023-03-22 16:00:13 -0300

    Pacote MethodOverride

--- nodeJs-MongoDb/to-do-list/app.js
+++ nodeJs-MongoDb/to-do-list/app.js
@@ -3,6 +3,7 @@ const path = require('path')
 
 const checkListRouter = require('./src/routes/checklist')
 const rootRouter = require('./src/routes/index')
+const methodOVerride = require('method-override')
 
 require('./config/database')
 
@@ -10,6 +11,7 @@ require('./config/database')
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(methodOVerride('_method'))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
