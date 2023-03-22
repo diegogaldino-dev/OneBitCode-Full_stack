6ab9f9c3489e6f3dd7d2222d3bf16260eb2c5154
Diego Galdino <diegogaldino-dev@gmail.com>
2023-03-22 13:41:28 -0300

    Enviando dados de um formulário com POST

--- nodeJs-MongoDb/to-do-list/src/routes/checklist.js
+++ nodeJs-MongoDb/to-do-list/src/routes/checklist.js
@@ -14,14 +14,24 @@ router.get('/', async (req, res) => {
   }
 })
 
+router.get('/new', async (req, res) => {
+  try {
+    let checklist = new Checklist()
+    res.status(200).render('checklists/new', { checklist: checklist })
+  } catch (error) {
+    res.status(500).render('pages/error', {error: 'Erro ao carregar o formulario'})
+  }
+})
+
 router.post('/', async (req, res) => {
-  let {name} = req.body
+  let { name } = req.body.checklist
+  let checklist = new Checklist({name})
   
   try {
-    let checklist = await Checklist.create({ name })
-    res.status(200).json(checklist)    
+    await Checklist.save()
+    res.redirect('/checklists')   
   } catch (error) {
-    res.status(422).json(error)    
+    res.status(422).render('checklists/new', { checklists: {...checklist, error}})
   }
   
 })
@@ -31,7 +41,7 @@ router.get('/:id', async (req, res) => {
     let checklist = await Checklist.findById(req.params.id) 
     res.status(200).render('checklists/show', { checklist: checklist })  
   } catch (error) {
-    res.status(200).render('pages/error', {error: 'Erro ao exibir as Listas de tarefas'})   
+    res.status(500).render('pages/error', {error: 'Erro ao exibir as Listas de tarefas'})   
   }
 })
 
